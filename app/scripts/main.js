0da59fa8b1db703eb44599dc740172ab07f1e44b
John Ellison <info@johnwebsites.com>
2015-09-20 19:17:45 +0100

    add typographic scale and begin styling header and nav bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+(function($){
+  'use strict';
+  console.log('\'Allo \'Allo!');
+
+  // For mobile, hide menu on open
+  $('.home-nav--list').hide();
+
+}(jQuery));
+
