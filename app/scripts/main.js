ca1cd7e28893f8253eb469740a4e4b83733108b3
John Ellison <info@johnwebsites.com>
2015-09-27 13:04:51 +0100

    style home-nav for mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,5 +5,13 @@
   // For mobile, hide menu on open
   $('.home-nav--list').hide();
 
+  // Toggle Menu
+  $('.menu-label').click(function(e){
+    e.preventDefault();
+    $('.home-nav--list').slideToggle(function(){
+      $('.menu-icon').toggleClass('is-open');
+    });
+  });
+
 }(jQuery));
 
