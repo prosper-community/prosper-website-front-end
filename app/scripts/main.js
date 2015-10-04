723e08f6e9db9332fa0c0fb6bca29c30d056abc2
John Ellison <info@johnwebsites.com>
2015-10-04 20:02:24 +0100

    make site-header fixed position

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -30,11 +30,41 @@
     });
   });
 
-
+  // When user clicks a link in the home-nav bar, smoothScroll
   $('.home-nav--list a').smoothScroll();
+
+  // When user clicks button (call to action), smoothScroll and focus input
   $('.button').smoothScroll({
     afterScroll: function() {
       $('input[name="your-name"]').focus();
     }
+  });
+
+  $('input[type="submit"]').click(function(e){
+    e.preventDefault();
+    // Vars passed into data for ajax call
+    var $sender = $('input[name="your-name"]').val();
+    var $email = $('input[name="your-email"]').val();
+    var $message = $('textarea[name="your-reason"]').val();
+    var $form = $('.l-join-form');
+
+    $.ajax({
+      url: "//formspree.io/admin@refugee.community",
+      method: "POST",
+      data: {
+        sender: $sender,
+        email: $email,
+        message: $message
+      },
+      dataType: "json",
+      success: function(data) {
+        $form.fadeOut();
+        var $formHeader = $('.l-join-wrapper h2');
+        $formHeader.html('Request Submitted.').addClass('success');
+      },
+      error: function(data) {
+        alert("sorry! we've had a server error. please try again later." + data);
+      }
+    })
   });
 }(jQuery));
