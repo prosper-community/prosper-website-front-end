bb826f36ffe13cc1a82f69e57e5e0c6a825c8457
John Ellison <info@johnwebsites.com>
2015-12-11 17:25:05 +0000

    fix personImage helper and add additional Academy content

--- src/helpers/personImage.js
+++ src/helpers/personImage.js
@@ -1,7 +1,7 @@
 module.exports = function(text) {
   var linkFriendly = text.toString().toLowerCase().split(' ').join('-');
 
-  var img = '<img src="assets/' + linkFriendly + '/.jpg" data-interchange="[assets/img/' + linkFriendly + '.jpg, small], [assets/img/' + linkFriendly + '@2x.jpg, retina]">';
+  var img = '<img src="assets/img/' + linkFriendly + '.jpg" data-interchange="[assets/img/' + linkFriendly + '.jpg, small], [assets/img/' + linkFriendly + '@2x.jpg, retina]">';
 
   return img;
 }
